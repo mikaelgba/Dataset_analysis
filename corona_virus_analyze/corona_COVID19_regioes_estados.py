c8367813e9b635fbc68c857a7b441075dad86ee3
mikaelgba <mikaelgba@gmail.com>
2020-04-21 11:26:39 -0300

    refactory

--- corona_virus_analyze/corona_COVID19_regioes_estados.py
+++ corona_virus_analyze/corona_COVID19_regioes_estados.py
@@ -7,5 +7,15 @@ estados = ["Acre", "Amapá", "Amazonas", "Pará", "Rondônia",
            "Espírito Santo", "Minas Gerais", "Rio de Janeiro", "São Paulo",
            "Paraná", "Rio Grande do Sul", "Santa Catarina"]
 
+regions = ["Norte","Nordeste","Centro-Oeste","Sudeste","Sul"]
+
 for i in estados:
     method.listar_dados_estado(i)
+
+for i in regions:
+    
+    regiao = method.selecionar_regiao(i)
+    print(regiao)
+    dias, casos, mortes = method.criar_listas(regiao)    
+    method.gerar_grafico(dias, casos,"Casos na região " + i,"Casos","Dias")
+    method.gerar_grafico(dias, mortes,"Mortes na região " + i,"Mortes","Dias")
